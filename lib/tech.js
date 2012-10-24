borschik/borschik

commit 1a5acdd3cde209295f00352a072e2132fa785054

Fixed bugs triggering on windows
Fixed tech resolving
Fixed pathTo, pathFrom methods to return URI on any platform

lib/tech.js
@@ -2,6 +2,7 @@ var Q = require('q'),
     FS = require('./fs'),
     QFS = require('q-fs'),
     PATH = require('path'),
+    URL = require('url'),
     INHERIT = require('inherit');
 
 exports.Tech = INHERIT({
@@ -100,11 +101,14 @@ exports.Tech = INHERIT({
             },
 
             pathTo: function(path) {
-                return PATH.resolve(PATH.dirname(this.path), path);
+                return URL.resolve(PATH.dirname(this.path), path);
             },
 
             pathFrom: function(path) {
-                return PATH.relative(PATH.dirname(path), this.path);
+                var p = PATH.relative(PATH.dirname(path), this.path);
+		if (PATH.sep === '\\') p = p.replace(/\\/g, '/');
+
+		return p;
             }
 
         })

commit fb0439a82de42fab8f850239c2c5a26ca4989ddb

Fixed bug introduced with 1a5acdd3cde209295f00352a072e2132fa785054
URL.resolve can't be used instead of PATH.resolve

lib/tech.js
@@ -2,8 +2,8 @@ var Q = require('q'),
     FS = require('./fs'),
     QFS = require('q-fs'),
     PATH = require('path'),
-    URL = require('url'),
-    INHERIT = require('inherit');
+    INHERIT = require('inherit'),
+    U = require('./util');
 
 exports.Tech = INHERIT({
 
@@ -101,14 +101,11 @@ exports.Tech = INHERIT({
             },
 
             pathTo: function(path) {
-                return URL.resolve(PATH.dirname(this.path), path);
+                return U.pathToUnix(PATH.resolve(PATH.dirname(this.path), path));
             },
 
             pathFrom: function(path) {
-                var p = PATH.relative(PATH.dirname(path), this.path);
-                if (PATH.sep === '\\') p = p.replace(/\\/g, '/');
-
-		return p;
+                return U.pathToUnix(PATH.relative(PATH.dirname(path), this.path));
             }
 
         })

lib/util.js
@@ -1,0 +1,6 @@
+var PATH = require('path');
+
+exports.pathToUnix = function(path) {
+    if (PATH.sep === '\\') return path.replace(/\\/g, '/');
+    return path;
+}
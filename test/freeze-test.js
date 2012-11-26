eff57ddcf933c7ee3f3b21ec08871ca6c2e70c3c
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2012-11-26 20:03:12 +0200

    UglifyJS test for js+coffee tech

--- test/freeze-test.js
+++ test/freeze-test.js
@@ -221,25 +221,28 @@ describe('CSSO yes, tech css-fast', function() {
     testFreeze('css-fast', 'csso_test', 'a.css', '_a.css', 'ok_css.css', 'yes');
 });
 
-describe('UglifyJS yes, tech js', function() {
-    var dir = 'uglifyjs_test',
-        inPath = 'test.js',
-        outPath = '_test.js',
-        okPath = 'ok_js.js';
-
+function testJS(tech, dir, inPath, outPath, okPath, minimize) {
     inPath = PATH.resolve(PATH.join(__dirname, dir, inPath));
     outPath = PATH.resolve(PATH.join(__dirname, dir, outPath));
     okPath = PATH.resolve(PATH.join(__dirname, dir, okPath));
 
     before(function() {
-        return BORSCHIK.api({ tech: 'js', input: inPath, output: outPath });
+        return BORSCHIK.api({ tech: tech, input: inPath, output: outPath, minimize: minimize });
     });
 
-    it('UglifyJS ok', function() {
+    it('UglifyJS, tech ' + tech + ' ok', function() {
         ASSERT.equal(readFile(outPath).toString(), readFile(okPath).toString());
     });
 
     after(function() {
         FS.unlinkSync(outPath);
     });
+}
+
+describe('UglifyJS yes, tech js', function() {
+    testJS('js', 'uglifyjs_test', 'test.js', '_test.js', 'ok_js.js', 'yes');
+});
+
+describe('UglifyJS yes, tech js+coffee', function() {
+    testJS('js+coffee', 'uglifyjs_test', 'test.coffee', '_test.js', 'ok_jscoffee.js', 'yes');
 });

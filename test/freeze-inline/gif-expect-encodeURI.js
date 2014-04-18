39155d1fff1ddccd77ef78e8c11d858d84e56cf6
Sergey Belov <peimei@ya.ru>
2014-04-18 15:41:28 +0400

    Add tests for encodeURI inlining method

---
+++ test/freeze-inline/gif-expect-encodeURI.js
@@ -0,0 +1,1 @@
+var html = '<img src="' + "data:image/gif,GIF89a%01%00%01%00%EF%BF%BD%00%00%EF%BF%BD%EF%BF%BD%EF%BF%BD%00%00%00!%EF%BF%BD%04%01%00%00%00%00,%00%00%00%00%01%00%01%00%00%02%02D%01%00;" + '"/>';

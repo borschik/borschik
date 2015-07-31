e0a91ad3e307af2fa39cd121b4f45949486c756d
Vladimir Rakchaev <rakchaev@ya.ru>
2015-07-31 12:11:42 +0300

    Add tests for inlining limit

---
+++ test/freeze-inline/gif-expect-base64-limit.js
@@ -0,0 +1,1 @@
+var html = '<img src="' + "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" + '"/>';
---
+++ test/freeze-inline/svg-expect-base64-limit.js
@@ -0,0 +1,1 @@
+var html = '<img src="' + "//yandex.st/prj/_4NWdbvGZ5GcESaCVriDR_Eznhy8.svg" + '"/>';
